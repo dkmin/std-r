f68999dd0bf3c7d3801ecb42889099f10f8cb9ea
D.K. Min <dk.min7@gmail.com>
2016-10-25 19:56:53 +0900

    set User Acc.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,25 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor'; // Add Meteor Package <==
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx'; // Add compo def
 
-// App component - represents the whole app
+// App compo => 예제 앱 전체를 여기에 정의
 class App extends Component {
+
   handleSubmit(event) {
     event.preventDefault();
 
-    // Find the text field via the React ref
+    // ref="textInput" 를 찾아서 value 를 text 에 대입
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
     Tasks.insert({
       text,
-      createdAt: new Date(), // current time
+      createdAt: new Date(),           // current time
+      owner: Meteor.userId(),          // id of logged in user <==
+      username: Meteor.user().username // username of logged in user <==
     });
 
-    // Clear form
+    // DB instert 후 폼에 있는 값을 ''로 지위줌
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
@@ -35,13 +40,17 @@ class App extends Component {
         <header>
           <h1>Todo List</h1>
 
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form>
+          <AccountsUIWrapper />  {/* Add compo tag  */}
+
+            { this.props.currentUser ? // 로그인 했을 때만 보이도록 제어문 추가 <==
+              <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                <input
+                  type="text"
+                  ref="textInput"
+                  placeholder="Type to add new tasks"
+                />
+              </form> : ''
+            }
 
         </header>
 
@@ -56,10 +65,13 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  currentUser: PropTypes.object,  // 데이터 형식이 object 임을 정의 <==
 };
 
 export default createContainer(() => {
   return {
       tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+      currentUser: Meteor.user(),  // App compo 에서 사용하도록 등록 <==
+
   };
 }, App);
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,13 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
+import {Tasks} from '../api/tasks.js';
 
-import { Tasks } from '../api/tasks.js';
-
-// Task component - represents a single todo item
+// Task compo - single todo item을 여기에 정의
 export default class Task extends Component {
+
   toggleChecked() {
-    // Set the checked property to the opposite of its current value
+    // checked property를 반대 값으로 update 한다. !=반대
     Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
+      $set: {
+        checked: !this.props.task.checked
+      }
     });
   }
 
@@ -16,9 +18,11 @@ export default class Task extends Component {
   }
 
   render() {
-    // Give tasks a different className when they are checked off,
-    // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    // CSS class 는 React에서 className 으로 사용한다.
+    // 그래서 CSS style 을 쉽게 변경할 수 있다. 목적) className='checked'
+    const taskClassName = this.props.task.checked
+      ? 'checked'
+      : '';
 
     return (
       <li className={taskClassName}>
@@ -26,21 +30,19 @@ export default class Task extends Component {
           &times;
         </button>
 
-        <input
-          type="checkbox"
-          readOnly
-          checked={this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
-        />
+        <input type="checkbox" readOnly checked={this.props.task.checked} onClick={this.toggleChecked.bind(this)}/>
 
-        <span className="text">{this.props.task.text}</span>
+        {/* 아래 3줄추가 = text 앞에 username을 보여준다 */}
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
       </li>
     );
   }
 }
 
 Task.propTypes = {
-  // This component gets the task to display through a React prop.
-  // We can use propTypes to indicate it is required
-  task: PropTypes.object.isRequired,
+  // Task compo 는 App 에서 상속 받은 task 를 사용한다.
+  // propTypes 는 Data type(자료형)을 정의하는 용도로 사용한다.
+  task: PropTypes.object.isRequired
 };
